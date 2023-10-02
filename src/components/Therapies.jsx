1b598fc7745bbc2f63f70f0fbd3499393a560d3b
RonnySk <ronny_sk@hotmail.com>
2023-10-02 17:28:35 +0200

    add style 1024px therapie.jsx

--- src/components/Therapies.jsx
+++ src/components/Therapies.jsx
@@ -5,24 +5,28 @@ export default function Therapies() {
   return (
     <div className="container-therapies">
       <h2>Áreas de Atendimento</h2>
-      <img className="logo2" src={logo2} alt="foto profissional de Soraya" />
-      <div className="therapies">
-        <ul>
-          <li>Motricidade (global e fina);</li>
-          <li>
-            Sensorial (tátil, vestibular, propriocetivo, auditivo, olfativo,
-            visual, gustativo);
-          </li>
-          <li>Interação social;</li>
-          <li>Perturbação do Espectro do Autismo;</li>
-          <li>Perturbação do Desenvolvimento Intelectual;</li>
-          <li>Perturbação de Défice de Atenção/Hiperatividade;</li>
-          <li>Atraso Global do Desenvolvimento;</li>
-          <li>Perturbação do Desenvolvimento da Coordenação;</li>
-          <li>Disfunção de Integração Sensorial;</li>
-          <li>Alterações genéticas (síndromes);</li>
-          <li>Seletividade alimentar.</li>
-        </ul>
+      <div className="subcontainer-therapies">
+        <img className="logo2" src={logo2} alt="foto profissional de Soraya" />
+        <div className="therapies">
+          <ul>
+            <li>Motricidade (global e fina)</li>
+            <li>
+              Sensorial (tátil, vestibular, propriocetivo, auditivo, olfativo,
+              visual, gustativo)
+            </li>
+            <li>Interação social</li>
+            <li>Perturbação do Espectro do Autismo</li>
+            <li>Perturbação do Desenvolvimento Intelectual</li>
+            <li>Perturbação de Défice de Atenção/Hiperatividade</li>
+          </ul>
+          <ul>
+            <li>Atraso Global do Desenvolvimento</li>
+            <li>Perturbação do Desenvolvimento da Coordenação</li>
+            <li>Disfunção de Integração Sensorial</li>
+            <li>Alterações genéticas (síndromes)</li>
+            <li>Seletividade alimentar</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
