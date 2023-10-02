577d36a6356c6e94f4fac1e1ff998dda584ad6f8
RonnySk <ronny_sk@hotmail.com>
2023-10-02 17:10:49 +0200

    styöe aboutTo 1024px

--- src/components/AboutTO.jsx
+++ src/components/AboutTO.jsx
@@ -5,27 +5,31 @@ export default function AboutTO() {
   return (
     <div className="container-aboutTo">
       <h2> O que é Terapia Ocupacional?</h2>
-      <div className="second-section2">
+      <div className="subcontainer-aboutTo">
         <img
           className="foto-puzzle"
           src={puzzle}
           alt="foto duas criancas com um puzzle"
         />
-        <p>
-          A Terapia Ocupacional visa promover a participação dos pacientes de
-          forma autónoma e independente em todas as áreas de ocupação
-          (atividades da vida diária, escola, sono, participação social, lazer…)
-        </p>
-        <p>
-          A intervenção do Terapeuta Ocupacional passa pelo desenvolvimento de
-          competências em défice (cognitivas, motoras, emocionais, sociais ou
-          sensoriais), ou pela adaptação do ambiente.
-        </p>
-        <p>
-          O brincar é o principal meio de intervenção do Terapeuta Ocupacional
-          pediátrico, sendo este facilitador e motivador para o alcance dos
-          objetivos terapêuticos.
-        </p>
+
+        <div className="subcontainer-aboutTo-p">
+          <p>
+            A Terapia Ocupacional visa promover a participação dos pacientes de
+            forma autónoma e independente em todas as áreas de ocupação
+            (atividades da vida diária, escola, sono, participação social,
+            lazer…)
+          </p>
+          <p>
+            A intervenção do Terapeuta Ocupacional passa pelo desenvolvimento de
+            competências em défice (cognitivas, motoras, emocionais, sociais ou
+            sensoriais), ou pela adaptação do ambiente.
+          </p>
+          <p>
+            O brincar é o principal meio de intervenção do Terapeuta Ocupacional
+            pediátrico, sendo este facilitador e motivador para o alcance dos
+            objetivos terapêuticos.
+          </p>
+        </div>
       </div>
     </div>
   );
