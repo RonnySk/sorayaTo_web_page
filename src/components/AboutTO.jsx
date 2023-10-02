2e0bd2b3b49d9d956732bcc0b4f008a4f6afe9f2
RonnySk <ronny_sk@hotmail.com>
2023-10-02 12:19:13 +0200

    add and update aboutTO

--- src/components/AboutTO.jsx
+++ src/components/AboutTO.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import sorayaPerfilImg from "../images/soraya_perfil.png.jpg";
+import puzzle from "../images/puzzle.png";
 
 export default function AboutTO() {
   return (
-    <div className="section2">
-      <h2> SOBRE MIM</h2>
+    <div className="container-aboutTo">
+      <h2> O que é Terapia Ocupacional?</h2>
       <div className="second-section2">
         <img
-          className="foto-perfil"
-          src={sorayaPerfilImg}
-          alt="foto profissional de Soraya"
+          className="foto-puzzle"
+          src={puzzle}
+          alt="foto duas criancas com um puzzle"
         />
-        <div className="about-me">
-          <h3>Soraya Leone de Moraes</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
-        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
       </div>
     </div>
   );
